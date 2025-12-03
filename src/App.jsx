fdd6a8f104f22c92f904f294df5dcb2013d0cf4b
Agustin Schenfelt Ruiz <schenfeltagustin@gmail.com>
2025-12-03 20:19:11 -0300

    arreglo imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"; 
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 // Componentes Shell
 import Layout from "./components/Layout/Layout";
