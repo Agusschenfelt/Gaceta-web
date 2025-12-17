d23d8fc24a735085c41f1de706ee9b5df45fa307
Agustin Schenfelt Ruiz <schenfeltagustin@gmail.com>
2025-12-17 18:10:01 -0300

    vuelve popup y lock

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ export default function App() {
 
 
         <HypeLock />
+        <EventPopup />
         
         <Suspense fallback={<LoadingScreen />}>
           <Routes>
--- src/components/HypeLock.jsx
+++ src/components/HypeLock.jsx
@@ -5,7 +5,7 @@ import { useGSAP } from "@gsap/react";
 import { useRef, useState, useLayoutEffect } from "react";
 
 // --- CONFIGURACIÓN ---
-const IS_LOCKED_INITIALLY = false; 
+const IS_LOCKED_INITIALLY = true; 
 const ACCESS_PASSWORD = "GACETAZO"; 
 
 export default function HypeLock() {
